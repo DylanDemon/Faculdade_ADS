41473ea8e3bd117b1f5183f5af33f8a7d50062ca
DylanDemon <thegokugamer76@gmail.com>
2026-06-01 19:15:59 -0300

    comeca apresentacao 4

---
+++ 2_Periodo/Trabalho Padrao/Apresentacao 4/Scripts/1.LocalStorage.js
@@ -0,0 +1,34 @@
+const S_Favoritos = []
+const S_Deck1 = []
+const S_Deck2 = []
+const S_Deck3 = []
+function PrincipalStorage(S_Favoritos, S_Deck1, S_Deck2, S_Deck3){
+    try {  
+        localStorage.setItem("Favoritos",JSON.stringify(S_Favoritos || []));
+        localStorage.setItem("Deck1",JSON.stringify(S_Deck1 || []));
+        localStorage.setItem("Deck2",JSON.stringify(S_Deck2 || []));
+        localStorage.setItem("Deck3",JSON.stringify(S_Deck3 || []));
+    } 
+    catch (erro) {
+        console.error("DEU UM ERRO NO FUNÇAO >> salvarStorage << LOCALIZADO NA LocalStorage.js" + erro.message);
+    }
+  
+}
+function CarregarStorage(){
+    try {
+        /* typenumber */
+    const Favoritos = JSON.parse(localStorage.getItem("Favoritos")) || [];
+    const Deck1 = JSON.parse(localStorage.getItem("Deck1")) || [];
+    const Deck2 = JSON.parse(localStorage.getItem("Deck2")) || [];
+    const Deck3 = JSON.parse(localStorage.getItem("Deck3")) || [];
+
+    console.log("Carregou Tudo Certo");
+
+    return { Favoritos, Deck1, Deck2, Deck3};
+    } 
+    catch (erro) {
+        console.error("DEU UM ERRO NO FUNÇAO >> mostrarTamanhoHeader << LOCALIZADO NA MostrarTamanho.js" + erro.message);
+        return {Favoritos: [], Deck1: [], Deck2: [], Deck3: [] };
+    }
+}
+export{PrincipalStorage, CarregarStorage};
---
+++ 2_Periodo/Trabalho Padrao/Apresentacao 4/Scripts/4.PokemonsMostrar.js
@@ -0,0 +1,93 @@
+//#region Imports
+import pokemons from "./1.ListaPokemons.js";
+import { CarregarStorage } from "./1.LocalStorage.js";
+import {f_Favoritos } from "./2.Favoritos.js";
+import {PokeLimits} from "./3.PokeLimits.js";
+//#endregion
+const Card_Info = document.getElementById('card_info');
+const container = document.getElementById('poke_contain');
+const dados = CarregarStorage();
+
+function PokemonsMostrar(){
+    try {
+        if (!container) {
+            console.error("Erro: O elemento #poke_contain nao foi encontrado no HTML!");
+            return;
+        }       
+        let Lista= PokeLimits();
+        container.innerHTML = "";
+
+            Lista.forEach(pokemon => { 
+                const card = document.createElement('div');
+                card.classList.add('card');
+
+                if(dados.Favoritos.includes(pokemon.ID)) pokemon.icone = "./CSS/Imagems_Extras/Favorito_1.png"
+                else pokemon.icone = "./CSS/Imagems_Extras/Favorito_0.png"
+
+                card.innerHTML = `
+                    <div class="card_pokemon" style="background-image: url('${pokemon.card}');">
+                        <div class="grupo_texto">
+                            <h1 class="Texto_ID">${pokemon.ID}</h1>
+                            <h1 class="Texto_2">#</h1>
+                        </div>
+                        <h1 class="Texto_Pokemon">${pokemon.nome}</h1>
+                        <img src="${pokemon.imagem}" alt="${pokemon.ID}" class="pokemons">
+                        <img src=${pokemon.icone} alt="" class="Favorito">
+                    </div>
+                `;
+                card.addEventListener('click', () =>{
+                    if(Card_Info){
+                        Card_Info.style.display = "block";
+                        Card_Info.innerHTML = `
+                            <div class="Back_card">
+                                <button id="btn_sair">X</button>
+                                <div class="area_foto">
+                                    <P class="pokemonID">#${pokemon.ID}</P>
+                                    <img src="${pokemon.imagem}" alt="${pokemon.ID}" class="pokemon_cardinfo"> 
+                                    <p class="pokemonGen">${pokemon.Gen}</p>
+                                </div>
+                                <div class="info_pokemon">
+                                    <h1 class="pokemon_nome">${pokemon.nome.toUpperCase()}</h1>
+                                    <h2 class="pokemonTipo">${pokemon.tipo}</h2>
+                                    <P class="pokemonNivel">Nivel: ${pokemon.nivel}</P>
+                                    <P class="pokemonTamanho">Tamanho: ${pokemon.tamanho}</P>
+                                    <p class="pokemonPeso">Peso: ${pokemon.peso}</p>
+                                    <P class="pokemonHab_principal">Habilidades Principais: ${pokemon.hab_principal}</P>
+                                    <p class="pokemonHab_oculta">Habilidades Ocultas: ${pokemon.hab_oculta}</p>
+                                    <P class="pokemonFraqueza">Fraquezas: ${pokemon.fraqueza}</P>
+                                    <p class="pokemonsobre">${pokemon.sobre}</p>
+                                    <P class="pokemonEvolucoes">Evolucao: ${pokemon.evolucoes}</P>
+                                </div>
+
+
+
+                            </div>
+                        `;
+                        Card_Info.querySelector('#btn_sair').addEventListener('click', (e) => {
+                            e.stopPropagation();
+                            Card_Info.style.display = "none";
+                        });
+                    } else {
+                        console.warn("Nao Achei O Card_Info")
+                    }
+                });
+
+                const botaoFavorito = card.querySelector('.Favorito');
+                botaoFavorito.addEventListener('click', (e) => {
+                    e.stopPropagation();
+                    f_Favoritos(botaoFavorito, pokemon)
+                });
+                container.appendChild(card);
+            })
+
+    } 
+    catch (erro) {
+        console.error("DEU UM ERRO NO FUNcAO >> PokemonsMostrar << LOCALIZADO NA PokemonsMostrar.js" + erro.stack);
+    }
+}
+export {container,PokemonsMostrar};
+
+
+
+
+
