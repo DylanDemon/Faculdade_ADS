2081ec72fb0b9b3c8fb8a4d1f5d33df9e6ebcb15
DylanDemon <thegokugamer76@gmail.com>
2026-06-01 21:21:01 -0300

    1.0

--- 2_Periodo/Trabalho Padrao/Apresentacao 4/main.js
+++ 2_Periodo/Trabalho Padrao/Apresentacao 4/main.js
@@ -1,14 +1,20 @@
 import {container, PokemonsMostrar} from './Scripts/4.PokemonsMostrar.js';
 import {PokeLimits, Pagina, pInput} from './Scripts/3.PokeLimits.js';
-import {botaoHome, botaoDeck, botaoLogin,botaoProximo, botaoAnterior, aperteiHome, aperteiDeck, aperteiLogin,aperteiProximo, aperteiAnterior} from './Scripts/3.Botoes.js';
+import {LocalPagina,aperteiProximo, aperteiAnterior} from './Scripts/3.Botoes.js';
 import { PrincipalStorage } from './Scripts/1.LocalStorage.js';
 import { mostrarTamanhoHeader } from './Scripts/1.MostrarTamanho.js';
 import { botao_Buscar, Texto_Buscar, Procurar} from './Scripts/2.Procurador.js';
 
+const botaoHome = document.getElementById("Botao_Home");
+const botaoDeck = document.getElementById("Botao_Deck");
+const botaoLogin = document.getElementById("Botao_Login");
+const botaoProximo = document.getElementById("Botao_Proximo");
+const botaoAnterior = document.getElementById("Botao_Anterior");
+
 function init(){    
-    if(botaoHome)     botaoHome.addEventListener("click", aperteiHome)
-    if(botaoDeck)     botaoDeck.addEventListener("click", aperteiDeck)
-    if(botaoLogin)    botaoLogin.addEventListener("click", aperteiLogin)
+    if(botaoHome)     botaoHome.addEventListener("click", (e) => LocalPagina("index"))
+    if(botaoDeck)     botaoDeck.addEventListener("click", (e) => LocalPagina("Deck"))
+    if(botaoLogin)    botaoLogin.addEventListener("click", (e) => LocalPagina("Login"))
     if(botaoProximo)  botaoProximo.addEventListener("click", aperteiProximo)
     if(botaoAnterior) botaoAnterior.addEventListener("click", aperteiAnterior)
 
